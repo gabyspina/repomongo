c5d9a868edee54e07c93493260d26990af319d0f
gabyspina <gabyspina@gmail.com>
2023-05-28 14:19:45 -0300

    agrego comentario

--- src/app.js
+++ src/app.js
@@ -12,6 +12,6 @@ app.use('/api/students', studentsRouter);
 
 mongoose.connect(
 	'mongodb+srv://gabyspina:gsp246813579@coderclaster.gnpohje.mongodb.net/colegio?retryWrites=true&w=majority'
-);
+); // Aca conecto mi base de datos con atlas y despues de la /pongo mi celección, que crée en atlas
 
 app.listen(8080, () => console.log('Server running on port 8080'));
